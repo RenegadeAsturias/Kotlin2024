7114863c4ca245badc750beb1d5f15b80598bcef
RenegadeAsturias <renegade.asturias@gmail.com>
2024-07-29 00:38:01 +0200

    Kotlin 2024

--- Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/FirstAppActivity.kt
+++ Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/FirstAppActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.renegade.aplicacion2024.R
 
 class FirstAppActivity : AppCompatActivity() {
@@ -12,8 +13,13 @@ class FirstAppActivity : AppCompatActivity() {
         setContentView(R.layout.activity_first_app)
         // Al arrancar la pantalla
         val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+        // Recupera el texto del EditText
+        // val strEtName = etName.text.toString()
+
         btnStart.setOnClickListener {
-            Log.i("renegade","Botón pulsado")
+            Log.i("renegade","Botón pulsado ${etName.text.toString()}")
         }
+
     }
 }
