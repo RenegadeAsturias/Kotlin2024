49fb4dcc82ab220920551f4c83cbb1c96c4362fc
RenegadeAsturias <renegade.asturias@gmail.com>
2024-07-28 23:57:02 +0200

    Kotlin 2024

--- Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/FirstAppActivity.kt
+++ Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/FirstAppActivity.kt
@@ -1,13 +1,19 @@
 package com.renegade.aplicacion2024.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.renegade.aplicacion2024.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
-
+        // Al arrancar la pantalla
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        btnStart.setOnClickListener {
+            Log.i("renegade","Botón pulsado")
+        }
     }
 }
