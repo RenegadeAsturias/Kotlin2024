d96a90f0ad588e54608c2fa0c30e7d078685f12c
RenegadeAsturias <renegade.asturias@gmail.com>
2024-07-31 00:53:22 +0200

    Kotlin 2024

---
+++ Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/MenuActivity.kt
@@ -0,0 +1,24 @@
+package com.renegade.aplicacion2024.firstapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.renegade.aplicacion2024.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        val btnAppSaludar = findViewById<AppCompatButton>(R.id.btnAppSaludar)
+
+        btnAppSaludar.setOnClickListener {
+            val intentAppSaludar = Intent(this, FirstAppActivity::class.java)
+            startActivity(intentAppSaludar)
+        }
+    }
+}
