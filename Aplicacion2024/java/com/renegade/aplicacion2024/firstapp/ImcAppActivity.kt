2100765eafef380de6a6d099e429826013f7c8dc
RenegadeAsturias <renegade.asturias@gmail.com>
2024-08-04 04:09:45 +0200

    Kotlin 2024

--- Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/ImcAppActivity.kt
+++ Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/ImcAppActivity.kt
@@ -3,16 +3,67 @@ package com.renegade.aplicacion2024.firstapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.renegade.aplicacion2024.R
 
 class ImcAppActivity : AppCompatActivity() {
+
+    private var isMaleSelected:Boolean = true
+    private var isFemaleSelected:Boolean = false
+
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_app)
 
-
-
+        initComponent()
+        initListeners()
+        initUI()
     }
+
+    private fun initComponent() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+    }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent:Boolean):Int {
+        val colorReference = if(isSelectedComponent) {
+            R.color.background_component_selected
+        } else {
+            R.color.background_component
+        }
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+
 }
+
