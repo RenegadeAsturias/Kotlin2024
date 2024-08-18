05784ca2eaff0b09b575864f294cd9169c0567eb
RenegadeAsturias <renegade.asturias@gmail.com>
2024-08-19 00:18:17 +0200

    Kotlin 2024

--- Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/ImcAppActivity.kt
+++ Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/ImcAppActivity.kt
@@ -1,12 +1,15 @@
 package com.renegade.aplicacion2024.firstapp
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.RangeSlider
 import com.renegade.aplicacion2024.R
 
 class ImcAppActivity : AppCompatActivity() {
@@ -16,6 +19,8 @@ class ImcAppActivity : AppCompatActivity() {
 
     private lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:RangeSlider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,8 @@ class ImcAppActivity : AppCompatActivity() {
     private fun initComponent() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
     }
 
     private fun initListeners() {
@@ -39,6 +46,11 @@ class ImcAppActivity : AppCompatActivity() {
         viewFemale.setOnClickListener {
             changeGender()
             setGenderColor()
+        }
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text = "$result cm"
         }
     }
 
