1fb4dde6ba896b3291e50c0ef0373d0b2452702e
RenegadeAsturias <renegade.asturias@gmail.com>
2024-09-02 00:18:48 +0200

    Kotlin 2024

--- Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/CategoriesViewHolder.kt
+++ Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/CategoriesViewHolder.kt
@@ -1,8 +1,31 @@
 package com.renegade.aplicacion2024.firstapp
 
 import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.renegade.aplicacion2024.R
 
 class CategoriesViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    private val tvCategoryName:TextView = view.findViewById(R.id.tvCategoryName)
+    private val divider:View = view.findViewById(R.id.divider)
+
+    fun render(taskCategory: TaskCategory) {
+        when(taskCategory) {
+            TaskCategory.Business -> {
+                tvCategoryName.text = "Personal"
+                divider.setBackgroundColor(ContextCompat.getColor(divider.context,R.color.todo_personal_category))
+            }
+            TaskCategory.Other -> {
+                tvCategoryName.text = "Negocios"
+                divider.setBackgroundColor(ContextCompat.getColor(divider.context,R.color.todo_business_category))
+            }
+            TaskCategory.Personal -> {
+                tvCategoryName.text = "Otros"
+                divider.setBackgroundColor(ContextCompat.getColor(divider.context,R.color.todo_other_category))
+            }
+        }
+    }
 
 }
--- Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/TodoActivity.kt
+++ Aplicacion2024/java/com/renegade/aplicacion2024/firstapp/TodoActivity.kt
@@ -5,12 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.renegade.aplicacion2024.R
 
 class TodoActivity : AppCompatActivity() {
 
-    private  lateinit var rvCategories: RecyclerView
+    private val categories = listOf(
+        TaskCategory.Personal,
+        TaskCategory.Business,
+        TaskCategory.Other
+    )
+
+    private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +32,9 @@ class TodoActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
-
+        categoriesAdapter = CategoriesAdapter(categories)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
     }
-
 
 }
